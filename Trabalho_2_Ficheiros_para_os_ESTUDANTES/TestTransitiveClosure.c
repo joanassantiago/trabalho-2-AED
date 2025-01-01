e5aba444c41ef979785c7de79b9d34affb8ca97e
Raquel Meira <raquelsmeira@ua.pt>
2025-01-01 13:19:36 +0000

    testes

--- Trabalho_2_Ficheiros_para_os_ESTUDANTES/TestTransitiveClosure.c
+++ Trabalho_2_Ficheiros_para_os_ESTUDANTES/TestTransitiveClosure.c
@@ -37,9 +37,9 @@ int main(void) {
   GraphCheckInvariants(tcdig01);
 
   // Reading a directed graph from file
-  FILE* file = fopen("DG_2.txt", "r");
-  Graph* dig03 = GraphFromFile(file);
-  fclose(file);
+  FILE* file1 = fopen("DG_2.txt", "r");
+  Graph* dig03 = GraphFromFile(file1);
+  fclose(file1);
   // Displaying in DOT format
   GraphDisplayDOT(dig03);
   printf("\n");
@@ -56,11 +56,91 @@ int main(void) {
 
   GraphCheckInvariants(tcdig03);
 
+  // Adding new graph dig04
+  Graph* dig04 = GraphCreate(8, 1, 0);
+  GraphAddEdge(dig04, 0, 1);
+  GraphAddEdge(dig04, 1, 2);
+  GraphAddEdge(dig04, 2, 3);
+  GraphAddEdge(dig04, 3, 4);
+  GraphAddEdge(dig04, 4, 5);
+  GraphAddEdge(dig04, 5, 6);
+  GraphAddEdge(dig04, 6, 7);
+  printf("The graph dig04:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(dig04);
+  printf("\n");
+
+  GraphCheckInvariants(dig04);
+
+  Graph* tcdig04 = GraphComputeTransitiveClosure(dig04);
+  InstrPrint();
+  InstrReset();
+  printf("The transitive closure of dig04:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(tcdig04);
+  printf("\n");
+
+  GraphCheckInvariants(tcdig04);
+
+  // Adding new graph dig05
+  Graph* dig05 = GraphCreate(5, 1, 0);
+  GraphAddEdge(dig05, 0, 1);
+  GraphAddEdge(dig05, 1, 2);
+  GraphAddEdge(dig05, 2, 0);
+  GraphAddEdge(dig05, 2, 3);
+  GraphAddEdge(dig05, 3, 4);
+  printf("The graph dig05:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(dig05);
+  printf("\n");
+
+  GraphCheckInvariants(dig05);
+
+  Graph* tcdig05 = GraphComputeTransitiveClosure(dig05);
+  InstrPrint();
+  InstrReset();
+  printf("The transitive closure of dig05:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(tcdig05);
+  printf("\n");
+
+  GraphCheckInvariants(tcdig05);
+
+  // Adding a large graph dig06
+  Graph* dig06 = GraphCreate(20, 1, 0);
+  for (int i = 0; i < 19; i++) {
+    for (int j = i + 1; j < 20; j++) {
+      GraphAddEdge(dig06, i, j);
+    }
+  }
+  printf("The large graph dig06:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(dig06);
+  printf("\n");
+
+  GraphCheckInvariants(dig06);
+
+  Graph* tcdig06 = GraphComputeTransitiveClosure(dig06);
+  InstrPrint();
+  InstrReset();
+  printf("The transitive closure of dig06:\n");
+  // Displaying in DOT format
+  GraphDisplayDOT(tcdig06);
+  printf("\n");
+
+  GraphCheckInvariants(tcdig06);
+
   GraphDestroy(&dig01);
   GraphDestroy(&dig03);
+  GraphDestroy(&dig04);
+  GraphDestroy(&dig05);
+  GraphDestroy(&dig06);
 
   GraphDestroy(&tcdig01);
   GraphDestroy(&tcdig03);
+  GraphDestroy(&tcdig04);
+  GraphDestroy(&tcdig05);
+  GraphDestroy(&tcdig06);
 
   return 0;
 }
